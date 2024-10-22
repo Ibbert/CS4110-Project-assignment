1b4885bc969e731ee045802c624d467f279d4ca8
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-22 09:14:18 +0200

    Update fir.c

--- InsertionSortHLSSynthesis/fir.c
+++ InsertionSortHLSSynthesis/fir.c
@@ -3,8 +3,7 @@
 #define N 8  // Number of elements to sort
 
 // Function to perform insertion sort
-void insertionSort(uint8_t arr[], uint8_t* output) 
-{
+void insertionSort(uint8_t arr[], uint8_t* output) {
 #pragma HLS INTERFACE mode=s_axilite port=insertionSort
 #pragma HLS INTERFACE mode=s_axilite port=arr
 #pragma HLS INTERFACE mode=s_axilite port=output
@@ -15,7 +14,6 @@ void insertionSort(uint8_t arr[], uint8_t* output)
         key = arr[i];
         j = i - 1;
 
-        // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
