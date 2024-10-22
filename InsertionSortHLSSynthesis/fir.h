ff79265504b07e992958520e8e4006df74b2c843
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-22 17:47:30 +0200

    Update and rename fir.h to is.h

--- InsertionSortHLSSynthesis/fir.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIR_H
-#define FIR_H
-
-#include <stdint.h>
-
-void insertionSort(uint8_t arr[], uint8_t* output);
-
-#endif
---
+++ InsertionSortHLSSynthesis/is.h
@@ -0,0 +1,8 @@
+#ifndef IS_H
+#define IS_H
+
+#include <stdint.h>
+
+void is(int tab[], int N);
+
+#endif
