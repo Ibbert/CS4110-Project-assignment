d1e673a42c76040f6cb40b35bc070c06e7f172be
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-23 13:25:42 +0200

    Update tb.c

--- InsertionSortHLSSynthesis/tb.c
+++ InsertionSortHLSSynthesis/tb.c
@@ -1,4 +1,4 @@
-#include "is.h"
+#include "insertionSort.h"
 #include <stdio.h>
 
 // I wrote this code, only God and I knew how it worked.
@@ -8,23 +8,26 @@
 int main() {
     int N = 12;  // Define the size of the array
     int arr[] = {9, 10, 2, 1, 0, 3, 5, 6, 4, 8, 11, 7};  // Example unsorted array
+    printf("Testing of array");
+    printf("\n");
 
     // Before sorting
-    printf("Before sorting: ");
+    printf("\nBefore sorting: ");
     for(int i = 0; i < N; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
+
 
     // Call the insertion sort function
-    is(arr, N);
+    insertionSort(arr, N);
 
     // After sorting
-    printf("After sorting: ");
+    printf("\nAfter sorting: ");
     for(int i = 0; i < N; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-
+    
     return 0;
 }
