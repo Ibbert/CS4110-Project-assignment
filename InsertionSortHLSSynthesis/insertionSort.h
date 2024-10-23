373ac910109465aee433fe6d24c2dce82ac1bc47
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-23 13:25:20 +0200

    Update and rename is.h to insertionSort.h

---
+++ InsertionSortHLSSynthesis/insertionSort.h
@@ -0,0 +1,8 @@
+#ifndef INSERTIONSORT_H
+#define INSERTIONSORT_H
+
+#include <stdint.h>
+
+int* insertionSort(int tab[], int N);
+
+#endif
--- InsertionSortHLSSynthesis/is.h
+++
@@ -1,8 +0,0 @@
-#ifndef IS_H
-#define IS_H
-
-#include <stdint.h>
-
-void is(int tab[], int N);
-
-#endif
