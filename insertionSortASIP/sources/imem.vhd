947aa0886c23c6de55612fe3ea67371fc54f2db1
ClaireVanoni <113844273+Cl-ai-re@users.noreply.github.com>
2024-10-24 17:38:51 +0200

    Update imem.vhd

--- insertionSortASIP/sources/imem.vhd
+++ insertionSortASIP/sources/imem.vhd
@@ -21,30 +21,14 @@ architecture arch of imem is
         of std_logic_vector(IMDATA_WIDTH-1 downto 0);
     
    constant instr_opcodes: rom_type:=(  
-    --  x"060380",  -- addr 00:     LD   R7,6      # 00000110(imm) 000(rs2=  ) 000 (rs1=  ) 111(rd=R7) 0000000 
-   --   x"001D01",  -- addr 01:     LD   R2,R7     # 00000000(imm) 000(rs2=  ) 111 (rs1=R7) 010(rd=R2) 0000001 
-	--  x"0A5C03",  -- addr 02:     ST   R2,10(R7) # 00001010(imm) 010(rs2=R2) 111 (rs1=R7) 000(rd=  ) 0000011
---	  x"001F85",  -- addr 03:     INC  R7        # 00000000(imm) 000(rs2=  ) 111 (rs1=R7) 111(rd=R7) 0000101
---	  x"00E886",  -- addr 04: L1: ADD  R1,R2,R7  # 00000000(imm) 111(rs2=R7) 010 (rs1=R2) 001(rd=R1) 0000110
---	  x"002487",  -- addr 05:     SUB  R1,R1,R1  # 00000000(imm) 001(rs2=R1) 001 (rs1=R1) 001(rd=R1) 0000111
---	  x"004888",  -- addr 06:     ORR  R1,R2,R2  # 00000000(imm) 010(rs2=R2) 010 (rs1=R2) 001(rd=R1) 0001000
---	  x"FF0489",  -- addr 07:     ORI  R1,R1,xFF # 11111111(imm) 000(rs2=  ) 001 (rs1=R1) 001(rd=R1) 0001001
---	  x"00448A",  -- addr 08:     ANR  R1,R1,R2  # 00000000(imm) 010(rs2=R2) 001 (rs1=R1) 001(rd=R1) 0001010
---	  x"00088B",  -- addr 09:     ANI  R1,R2,x00 # 00000000(imm) 000(rs2=  ) 010 (rs1=R2) 001(rd=R1) 0001011
---	  x"001F84",  -- addr 10:     DEC  R7        # 00000000(imm) 000(rs2=  ) 111 (rs1=R7) 111(rd=R7) 0000100
---	  x"F91C0D",  -- addr 11:     JRNZ R7,L1     # 11111001(imm) 000(rs2=  ) 111 (rs1=R7) 000(rd=  ) 0001101
---	  x"01080C",  -- addr 12:     JRZ  R7,L2     # 00000001(imm) 000(rs2=  ) 010 (rs1=R7) 000(rd=  ) 0001100
---	  x"0A0B82",  -- addr 13: L2: LD   R7,10(R2) # 00001010(imm) 000(rs2=  ) 010 (rs1=R2) 111(rd=R7) 0000010
---	  x"00000E",  -- addr 14: L3: J    L3        # 00000000(imm) 000(rs2=  ) 000 (rs1=  ) 000(rd=  ) 0001110
-
 
 	  x"050080",  -- addr 00:	LD   R1,5		# 00000101(imm) 000(rs2=  ) 000 (rs1=  ) 001(rd=R1) 0000000 
 	  x"010100",  -- addr 01:	LD   R2,1		# 00000001(imm) 000(rs2=  ) 000 (rs1=  ) 010(rd=R2) 0000000
 	  x"FF2803",  -- addr 02: 	ST   R1,-1(R2)	# 11111111(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011
 	  x"020080",  -- addr 03:	LD   R1,2		# 00000010(imm) 000(rs2=  ) 000 (rs1=  ) 001(rd=R1) 0000000 
-      x"002803",  -- addr 04: 	ST   R1,0(R2)	# 00000000(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011
+      	  x"002803",  -- addr 04: 	ST   R1,0(R2)	# 00000000(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011
 	  x"030080",  -- addr 05:	LD   R1,3		# 00000011(imm) 000(rs2=  ) 000 (rs1=  ) 001(rd=R1) 0000000 
-      x"012803",  -- addr 06: 	ST   R1,1(R2)	# 00000001(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011	  
+      	  x"012803",  -- addr 06: 	ST   R1,1(R2)	# 00000001(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011	  
 	  x"010080",  -- addr 07:	LD   R1,1		# 00000001(imm) 000(rs2=  ) 000 (rs1=  ) 001(rd=R1) 0000000 
 	  x"022803",  -- addr 08: 	ST   R1,2(R2)	# 00000010(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0000011  
 	  x"040080",  -- addr 09:	LD   R1,4		# 00000001(imm) 000(rs2=  ) 000 (rs1=  ) 001(rd=R1) 0000000 
@@ -72,18 +56,8 @@ architecture arch of imem is
 	  x"000905",  -- addr 1e: 	INC  R2			# 00000000(imm) 000(rs2=  ) 010 (rs1=R2) 010(rd=R2) 0000101
 	  x"F52810",  -- addr 1f: 	BLT  R2,R1,-11	# 11110101(imm) 001(rs2=R1) 010 (rs1=R2) 000(rd=  ) 0010000
 	  x"000100",  -- addr 20: 	LD   R2,0		# 00000000(imm) 000(rs2=  ) 000 (rs1=  ) 010(rd=R2) 0000000
-	  x"000982",  -- addr 21: 	LD   R3,0(R2)	# 00000000(imm) 000(rs2=  ) 010 (rs1=R2) 011(rd=R3) 0000010
-	  --x"00000E",  -- addr 25:	J	 0			# 00000000(imm) 000(rs2=  ) 000 (rs1=  ) 000(rd=  ) 0001110
-      
+	  x"000982",  -- addr 21: 	LD   R3,0(R2)	# 00000000(imm) 000(rs2=  ) 010 (rs1=R2) 011(rd=R3) 0000010      
 
-	  
-	  x"FFFFFF",  -- addr 15: (void)
-	  x"FFFFFF",  -- addr 16: (void)
-	  x"FFFFFF",  -- addr 17: (void)
-	  x"FFFFFF",  -- addr 18: (void)
-	  x"FFFFFF",  -- addr 19: (void)
-	  x"FFFFFF",  -- addr 20: (void)
-	  x"FFFFFF",  -- addr 21: (void)
 	  x"FFFFFF",  -- addr 22: (void)
 	  x"FFFFFF",  -- addr 23: (void)
 	  x"FFFFFF",  -- addr 24: (void)
@@ -92,21 +66,28 @@ architecture arch of imem is
 	  x"FFFFFF",  -- addr 27: (void)
 	  x"FFFFFF",  -- addr 28: (void)
 	  x"FFFFFF",  -- addr 29: (void)
-	  x"FFFFFF",  -- addr 15: (void)
-	  x"FFFFFF",  -- addr 16: (void)
-	  x"FFFFFF",  -- addr 17: (void)
-	  x"FFFFFF",  -- addr 18: (void)
-	  x"FFFFFF",  -- addr 19: (void)
-	  x"FFFFFF",  -- addr 20: (void)
-	  x"FFFFFF",  -- addr 21: (void)
-	  x"FFFFFF",  -- addr 22: (void)
-	  x"FFFFFF",  -- addr 23: (void)
-	  x"FFFFFF",  -- addr 24: (void)
-	  x"FFFFFF",  -- addr 25: (void)
-	  x"FFFFFF",  -- addr 26: (void)
-	  x"FFFFFF",  -- addr 27: (void)
-	  x"FFFFFF",  -- addr 28: (void)
-	  x"FFFFFF"  -- addr 29: (void)
+	  x"FFFFFF",  -- addr 2a: (void)
+	  x"FFFFFF",  -- addr 2b: (void)
+	  x"FFFFFF",  -- addr 2c: (void)
+	  x"FFFFFF",  -- addr 2d: (void)
+	  x"FFFFFF",  -- addr 2e: (void)
+	  x"FFFFFF",  -- addr 2f: (void)
+	  x"FFFFFF",  -- addr 30: (void)
+	  x"FFFFFF",  -- addr 31: (void)
+	  x"FFFFFF",  -- addr 32: (void)
+	  x"FFFFFF",  -- addr 33: (void)
+	  x"FFFFFF",  -- addr 34: (void)
+	  x"FFFFFF",  -- addr 35: (void)
+	  x"FFFFFF",  -- addr 36: (void)
+	  x"FFFFFF",  -- addr 37: (void)
+	  x"FFFFFF",  -- addr 38: (void)
+	  x"FFFFFF",  -- addr 39: (void)
+	  x"FFFFFF",  -- addr 3a: (void)
+	  x"FFFFFF",  -- addr 3b: (void)
+	  x"FFFFFF",  -- addr 3c: (void)
+	  x"FFFFFF",  -- addr 3d: (void)
+	  x"FFFFFF",  -- addr 3e: (void)
+	  x"FFFFFF"  -- addr 3f: (void)
 
 
 	  
