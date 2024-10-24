32629a5ed43f94c8ec240250985c7aa8f0ac47e8
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-24 17:56:14 +0200

    Update main.c

--- InsertionSortHLSSynthesis/main.c
+++ InsertionSortHLSSynthesis/main.c
@@ -1,48 +1,89 @@
-// MAIN REQUIRE CHANGE TO SUIT OUR PROJECT (Accoding to Adrian)
-// CHATGPT IS RECOMMENDED, BUT NOT ADVICED (According to Trond)
-// USE FIR-FILTER AS A TEMPLATE FOR THE HLS SYNTHESIS TASK (According to Professor Jose)
-// TL;DR - May God help us.
+// I wrote this code, only God and I knew how it worked.
+// Now, only God knows. 
+#define N_max 50 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <xparameters.h>
+#include "xinsertionsort.h"
 #include "platform.h"
-#include "xfir.h"
 #include "xil_printf.h"
-#include "xparameters.h"
 
-int main() {
-    print("\n\rFIR Filter\n\r>");
+int main()
+{
+    init_platform();
 
-    XFir fir =
+    printf("\nInsertion sorting\n\r");
+
+    XInsertionsort insertsort =
     {
         // XPAR_XFIR_0_BASEADDR might need to be changed. 
         // The parameter can be found in xparameters.h
         // For our board (Zynq 7000) the value should be 0x40000000
-        .Control_BaseAddress = XPAR_XFIR_0_BASEADDR, 
+        .Control_BaseAddress = XPAR_INSERTIONSORT_0_BASEADDR, 
         .IsReady = 0
     };
+    
+    XInsertionsort_Config* const config = XInsertionsort_LookupConfig(XPAR_XSLCR_0_DEVICE_ID);
+    const int ret = XInsertionsort_CfgInitialize(&insertsort, config);
+    Xil_AssertNonvoid(ret == XST_SUCCESS);
 
-    init_platform();
+    XInsertionsort_DisableAutoRestart(&insertsort);
 
-    XFir_Config* const config = XFir_LookupConfig(XPAR_XSLCR_0_DEVICE_ID);
-    const int ret = XFir_CfgInitialize( &fir, config);
-    Xil_AssertNonvoid( ret == XST_SUCCESS );
+    char c_table[2*N_max];
+    int c_cnt=0;
 
-    XFir_DisableAutoRestart(&fir);
+    while(1) {
+        const char c= inbyte();
+        if(c=='\r'){
+            printf("list:%s \n",c_table);
+            printf("one by one:\n");
+            int k=0;
+            while (c_table[k] !='\0'){                
+                printf("k:%d ;%c\n",k,c_table[k]);
+                k++;
+            }
 
-    while(1)
-    {
-        const char c = inbyte();
-        if(XFir_IsIdle(&fir))
-        {
-            XFir_Set_input_r(&fir, c);
-            XFir_Start(&fir);
+            int int_array[N_max];
+            for (k=c_cnt;k<2*N_max;k++){
+                c_table[k]=NULL;
+            }
 
-            while(!XFir_IsDone(&fir));
+            char* token= strtok(c_table,",");
+            int cnt=0;
+            while (token!=NULL & token!='\0'){
+                int_array[cnt]=atoi(token);
+                printf("newtoken:%s  :atoi:%d : cnt:%d\n",token,int_array[cnt],cnt);
+                token=strtok(NULL,",");
+                cnt++;
+            }
+            printf("cnt:%d!\n",cnt);
+            printf("blo\n");
+            if(XInsertionsort_IsIdle(&insertsort))
+            {
+                XInsertionsort_Set_tab(&insertsort, c);
+                XInsertionsort_Set_N(&insertsort,cnt);
+                XInsertionsort_Start(&insertsort);
+                while(!XInsertionsort_IsDone(&insertsort));
 
-            const uint8_t val = XFir_Get_output_r(&fir);
-            outbyte(val);
+                printf("From small to large:\n");
+                uint8_t* val = XInsertionsort_Get_return(&insertsort);
+                for (int i=0; i<cnt;i++){
+                    printf("n%d:%d\n",i,val[i]);
+                } 
+                *val = 0;                 
+                printf("end\n");
+                char c_table[50];
+                cnt=0;
+                c_cnt=0;
+            }
+        }else{
+            c_table[c_cnt]=c;
+            c_cnt++;
+            printf("strlen:%d ;len-1: %c\n",strlen(c_table),c_table[strlen(c_table)-1]);
         }
     }
-
     cleanup_platform();
     return 0;
 }
