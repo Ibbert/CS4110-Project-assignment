e66d50d1ff1e7a29bc98899210fcfd2e015d803f
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-25 11:08:44 +0200

    Update insertionSort.h

--- InsertionSortHLSSynthesis/insertionSort.h
+++ InsertionSortHLSSynthesis/insertionSort.h
@@ -2,8 +2,7 @@
 #define INSERTIONSORT_H
 
 #include <stdint.h>
-#define N 8
 
-void insertionSort(const uint8_t input[N],uint8_t output[N]);
+void insertionSort(const uint8_t input[], uint8_t output[]);
 
 #endif
