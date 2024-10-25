59766072fd5db9a8808f517d732994f288958506
Ibrahim <105546679+Ibbert@users.noreply.github.com>
2024-10-25 11:09:25 +0200

    Update tb.c

--- InsertionSortHLSSynthesis/tb.c
+++ InsertionSortHLSSynthesis/tb.c
@@ -1,33 +1,27 @@
-#include "insertionSort.h"
 #include <stdio.h>
+#include <stdint.h>
+#include "insertionSort.h"
 
-// I wrote this code, only God and I knew how it worked.
-// Now, only God knows. 
-// You will see full result when debugging or spamming runs.
+#define N 8  // Define the array size for testing
 
 int main() {
-    int N = 12;  // Define the size of the array
-    int arr[] = {9, 10, 2, 1, 0, 3, 5, 6, 4, 8, 11, 7};  // Example unsorted array
-    printf("Testing of array");
-    printf("\n");
+    uint8_t input_array[N] = {34, 7, 23, 32, 5, 62, 43, 17};
+    uint8_t output_array[N]; 
 
-    // Before sorting
-    printf("\nBefore sorting: ");
-    for(int i = 0; i < N; i++) {
-        printf("%d ", arr[i]);
+    printf("Unsorted array:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", input_array[i]);
     }
     printf("\n");
 
+    // Call function
+    insertionSort(input_array, output_array);
 
-    // Call the insertion sort function
-    insertionSort(arr, N);
-
-    // After sorting
-    printf("\nAfter sorting: ");
-    for(int i = 0; i < N; i++) {
-        printf("%d ", arr[i]);
+    printf("Sorted array:\n");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", output_array[i]);
     }
     printf("\n");
-    
+
     return 0;
 }
